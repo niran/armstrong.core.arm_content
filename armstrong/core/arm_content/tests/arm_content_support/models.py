1a31b70ed7e0d37f31e74e42e2650ede2beeac11
Niran Babalola <niran@niran.org>
2011-04-04 00:50:16 -0500

    Hook AuthorshipMixin into existing tests

--- armstrong/core/arm_content/tests/arm_content_support/models.py
+++ armstrong/core/arm_content/tests/arm_content_support/models.py
@@ -1,10 +1,12 @@
+from django.contrib.auth.models import User
 from django.db import models
 from polymorphic import PolymorphicModel
 
+from ...authorship.models import AuthorshipMixin
 from ...publication.models import PublicationMixin
 
 
-class BaseContent(PolymorphicModel, PublicationMixin):
+class BaseContent(PolymorphicModel, AuthorshipMixin, PublicationMixin):
     title = models.CharField(max_length=255)
 
 
@@ -14,3 +16,10 @@ class Article(BaseContent):
 
 class Video(BaseContent):
     youtube_id = models.CharField(max_length=30)
+
+
+class UserProfile(models.Model):
+    user = models.OneToOneField(User)
+
+    def get_absolute_url(self):
+        return '/users/%d/' % self.user.id
--- fabfile/__init__.py
+++ fabfile/__init__.py
@@ -4,11 +4,13 @@ from ._utils import *
 def test():
     settings = {
         'INSTALLED_APPS': (
+            'django.contrib.auth',
             'django.contrib.contenttypes',
             'armstrong.core.arm_content',
             'armstrong.core.arm_content.tests.arm_content_support',
         ),
         'ROOT_URLCONF': 'armstrong.core.arm_content.tests.arm_content_support.urls',
+        'AUTH_PROFILE_MODULE': 'arm_content_support.UserProfile',
     }
     run_tests(settings, 'arm_content_support', 'arm_content')
 
