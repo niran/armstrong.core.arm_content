2b2532efe53f9de5cc389cde4f4c0945c5d5c166
Niran Babalola <niran@niran.org>
2011-04-26 16:17:12 -0500

    Make arm_content.models a package for abstract models

--- armstrong/core/arm_content/models.py
+++
@@ -1,4 +0,0 @@
-# Here for Django's sake.
-#
-# This app doesn't ship with any models, just Mixins and Fields needed to
-# provide the basic functionality of Armstrong.
---
+++ armstrong/core/arm_content/models/__init__.py
@@ -0,0 +1,2 @@
+# This app doesn't ship with any concrete models, just abstract Models, Mixins
+# and Fields needed to provide the basic functionality of Armstrong.
